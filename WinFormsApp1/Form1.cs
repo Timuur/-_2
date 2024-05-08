d9583b0c15a269e8e2d9f46c76b58d3b0bfb2ddb
Timuur <timur2st@gmail.com>
2024-05-08 16:01:54 +0500

    Update Form1.cs

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -21,32 +21,6 @@ namespace WinFormsApp1
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            //var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
-
-            //var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            //udpSocket.Bind(udpEndPoint);
-            //while (true)
-            //{
-            //    label1.Text = "¬ведите сообщение:";
-            //    var message = textBox1.Text;
-
-            //    var serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
-            //    udpSocket.SendTo(Encoding.UTF8.GetBytes(message), serverEndPoint);
-
-            //    var buffer = new byte[256];
-            //    var size = 0;
-            //    var data = new StringBuilder();
-            //    EndPoint senderEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
-
-            //    do
-            //    {
-            //        size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
-            //        data.Append(Encoding.UTF8.GetString(buffer));
-            //    }
-            //    while (udpSocket.Available > 0);
-
-            //    label2.Text = data.ToString();
-            //}
 
         }
 
